e817c6480c248f9c39269d4e46ab6f40effd39a3
Tuan Bach Van <bachvtuan@gmail.com>
2022-11-15 14:11:42 +0700

    Debug

--- src/restore.ts
+++ src/restore.ts
@@ -38,8 +38,14 @@ async function run(): Promise<void> {
             repo: github.context.repo.repo,
             key: restoreKeys[0]
         })
-    
-        core.debug("Rest API: "  + JSON.stringify(data));
+        if (Array.isArray(data)){
+            core.debug("data is an array")
+            data.forEach((item) => {
+                core.debug(JSON.stringify(item))
+             });
+        }else{
+            core.debug("data is not an array")
+        }
     }
 
 
